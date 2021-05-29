a9d00d62204e9a4a76b2f5b965f049770f5dfe81
wangxin <wangxin@qq.com>
2021-05-29 12:38:40 +0800

    修改配置文件

---
+++ src/main/java/com/bjpowernode/utils/DataUtil.java
@@ -0,0 +1,26 @@
+package com.bjpowernode.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 日期工具类
+ */
+public class DataUtil {
+
+    public DataUtil(){
+    }
+
+
+
+   public static String  getTime(){
+       SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+       Date date=new Date();
+       return sdf.format(date);
+   }
+
+    public static void main(String[] args) {
+
+        System.out.println(getTime());
+    }
+}
